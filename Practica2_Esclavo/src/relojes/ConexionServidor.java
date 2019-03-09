28d8ab876fde1937e3f1a3a8414df85d53e6e244
Valerdi <dani_9_valerdi@hotmail.com>
2019-03-08 18:15:13 -0600

    Cliente listo

---
+++ Practica2_Esclavo/src/relojes/ConexionServidor.java
@@ -0,0 +1,44 @@
+
+package relojes;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import javax.swing.JTextField;
+
+/**
+ * Esta clase gestiona el envio de datos entre el cliente y el servidor.
+ * 
+ * @author Ivan Salas Corrales <http://programando-o-intentandolo.blogspot.com.es/>
+ */
+public class ConexionServidor implements ActionListener {
+    
+    private Socket socket; 
+    private String mensaje;
+    private DataOutputStream salidaDatos;
+    
+    public ConexionServidor(Socket socket, String mensaje) {
+        this.socket = socket;
+        this.mensaje = mensaje;
+        try {
+            this.salidaDatos = new DataOutputStream(this.socket.getOutputStream());
+        } catch (IOException ex) {
+            System.out.println("Error al crear el stream de salida : " + ex.getMessage());
+        } catch (NullPointerException ex) {
+            System.out.println("El socket no se creo correctamente. ");
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            salidaDatos.writeUTF(mensaje);
+            mensaje = "";
+        } catch (IOException ex) {
+            System.out.println("Error al intentar enviar un mensaje: " + ex.getMessage());
+        }
+    }
+
+}
--- Practica2_Esclavo/src/relojes/Reloj.java
+++ Practica2_Esclavo/src/relojes/Reloj.java
@@ -10,53 +10,81 @@ public class Reloj extends javax.swing.JFrame{
     BufferedReader entrada,teclado;
     PrintStream salida;
     String Reloj1;
-    Socket sk;
-    int pto = 5000;
-    String ip = "192.168.42.92";
+    Socket socket;
+    int puerto;
+    String host;
     RelojHilo contador;
       
        
     public Reloj() {
         initComponents();
         setTitle("Reloj Esclavo");
+        // Ventana de configuracion inicial
+        VentanaConfiguracion vc = new VentanaConfiguracion(this);
+        host = vc.getHost();
+        puerto = vc.getPuerto();
+        System.out.println("Te quieres conectar a " + host + " en el puerto " + puerto);
+        this.setVisible(true);
+        // Se crea el socket para conectar con el Sevidor del Chat
+        try {
+            socket = new Socket(host, puerto);
+        } catch (UnknownHostException ex) {
+            System.out.println("No se ha podido conectar con el servidor (" + ex.getMessage() + ").");
+        } catch (IOException ex) {
+            System.out.println("No se ha podido conectar con el servidor (" + ex.getMessage() + ").");
+        }
+        
+        // Accion para el boton enviar
+        btnPedir.addActionListener(new ConexionServidor(socket, "Dame hora"));
+        btnMandar.addActionListener(new ConexionServidor(socket, hora.getText() + ":" + minutos.getText()));
     }
     
-    public void conectar(){
-        contador = new RelojHilo(this.Lreloj);
-        try{
-            sk = new Socket(ip,pto);
-            entrada = new BufferedReader(new InputStreamReader(sk.getInputStream()));
-            //teclado = new BufferedReader(new InputStreamReader(System.in));
-            //String tec = teclado.readLine();
-            salida = new PrintStream(sk.getOutputStream());
-	   while(true){
-            salida.println("Hola");
-
-            Reloj1 = entrada.readLine();
-            Reloj1 = Reloj1.substring(2);
-            String[] numeros;
-            numeros = Reloj1.split(":");
-            contador.reloj1.setHora(Integer.parseInt(numeros[0]));
-            contador.reloj1.setMinuto(Integer.parseInt(numeros[1]));
-            contador.reloj1.setSegundo(Integer.parseInt(numeros[2]) + 1);
-	   }
-            //salida.close();
-            //sk.close();
-            //entrada.close();
-            
-        }catch(Exception e){
-            e.printStackTrace();
-        }; 
+    public void recibirMensajesServidor(){
+        // Obtiene el flujo de entrada del socket
+        DataInputStream entradaDatos = null;
+        String mensaje;
+        try {
+            entradaDatos = new DataInputStream(socket.getInputStream());
+        } catch (IOException ex) {
+            System.out.println("Error al crear el stream de entrada: " + ex.getMessage());
+        } catch (NullPointerException ex) {
+            System.out.println("El socket no se creo correctamente. ");
+        }
+        
+        // Bucle infinito que recibe mensajes del servidor
+        boolean conectado = true;
+        while (conectado) {
+            try {
+                mensaje = entradaDatos.readUTF();
+                System.out.println(mensaje);
+                contador = new RelojHilo(this.Lreloj);
+                String[] numeros;
+                numeros = mensaje.split(":");
+                contador.reloj1.setHora(Integer.parseInt(numeros[0]));
+                contador.reloj1.setMinuto(Integer.parseInt(numeros[1]));
+                contador.reloj1.setSegundo(Integer.parseInt(numeros[2]) + 1);
+            } catch (IOException ex) {
+                System.out.println("Error al leer del stream de entrada: " + ex.getMessage());
+                conectado = false;
+            } catch (NullPointerException ex) {
+                System.out.println("El socket no se creo correctamente. ");
+                conectado = false;
+            }
+        }
     }
+
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         Lreloj = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        textArea = new javax.swing.JTextArea();
+        btnPedir = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        hora = new javax.swing.JTextField();
+        minutos = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        btnMandar = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -64,15 +92,20 @@ public class Reloj extends javax.swing.JFrame{
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(204, 255, 51));
+        setResizable(false);
 
-        Lreloj.setFont(new java.awt.Font("Sitka Text", 1, 36)); // NOI18N
+        Lreloj.setFont(new java.awt.Font("Sitka Text", 1, 48)); // NOI18N
         Lreloj.setText("12:00:00");
 
-        jButton1.setText("jButton1");
+        btnPedir.setText("Pedir hora");
 
-        textArea.setColumns(20);
-        textArea.setRows(5);
-        jScrollPane1.setViewportView(textArea);
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setText("Cambiar hora:");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel2.setText(":");
+
+        btnMandar.setText("Cambiar hora");
 
         jMenu1.setText("File");
 
@@ -93,27 +126,43 @@ public class Reloj extends javax.swing.JFrame{
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(107, 107, 107)
-                        .addComponent(Lreloj))
+                        .addGap(131, 131, 131)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(hora, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(minutos, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(152, 152, 152)
-                        .addComponent(jButton1)))
-                .addContainerGap(146, Short.MAX_VALUE))
+                        .addGap(88, 88, 88)
+                        .addComponent(btnPedir)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnMandar)))
+                .addContainerGap(90, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(Lreloj, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(67, 67, 67)
-                .addComponent(Lreloj, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 10, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40)
+                .addComponent(Lreloj)
                 .addGap(18, 18, 18)
-                .addComponent(jButton1)
-                .addGap(38, 38, 38))
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(hora, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(minutos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnPedir)
+                    .addComponent(btnMandar))
+                .addContainerGap(56, Short.MAX_VALUE))
         );
 
         pack();
@@ -122,13 +171,16 @@ public class Reloj extends javax.swing.JFrame{
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Lreloj;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnMandar;
+    private javax.swing.JButton btnPedir;
+    private javax.swing.JTextField hora;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea textArea;
+    private javax.swing.JTextField minutos;
     // End of variables declaration//GEN-END:variables
       
         public static void main(String args[]) {
@@ -163,8 +215,7 @@ public class Reloj extends javax.swing.JFrame{
         });*/
         
         Reloj reloj = new Reloj();
-        reloj.conectar();
-        reloj.setVisible(true);
+        reloj.recibirMensajesServidor();
     }
     
 }
